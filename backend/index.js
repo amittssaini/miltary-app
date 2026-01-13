7a62fc7ba3025b57775219adf14ab9f892aab812
amittssaini <amitssaini999@gmail.com>
2026-01-13 18:55:55 +0530

    backenddb

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ console.log('DB IS CONNECTED');
 app.listen((process.env.PORT),()=>console.log("SERVER IS CONNECTED AT THE ",process.env.PORT))
 })
 .catch((error)=>console.log("error occur during connection ",error));
-//app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.get("/", (req, res) => {
   res.send("API is running...");
